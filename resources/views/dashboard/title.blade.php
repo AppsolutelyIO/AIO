2bfdf8e90f923039a8e1ac849f9439da7e3dbd4b
okchum <hi@okchum.com>
2026-03-26 01:58:39 +1300

    refactor(dashboard): use Admin constants for URLs in views
    Replace hardcoded URLs in dashboard title card and footer with
    Admin::WEBSITE_URL and Admin::GITHUB_URL constants. Remove unused
    documentation, extensions, and demo links from title card.

--- resources/views/dashboard/title.blade.php
+++ resources/views/dashboard/title.blade.php
@@ -3,7 +3,8 @@
         text-align: center;
         margin-bottom: 2.5rem;
     }
-    .dashboard-title .links > a {
+
+    .dashboard-title .links>a {
         padding: 0 25px;
         font-size: 12px;
         font-weight: 600;
@@ -12,10 +13,12 @@
         text-transform: uppercase;
         color: #fff;
     }
+
     .dashboard-title h1 {
         font-weight: 200;
         font-size: 2.5rem;
     }
+
     .dashboard-title .avatar {
         background: #fff;
         border: 2px solid #fff;
@@ -32,10 +35,8 @@
             <div class="text-center mb-1">
                 <h1 class="mb-3 mt-2 text-white">Appsolutely AIO</h1>
                 <div class="links">
-                    <a href="https://github.com/jqhph/aio" target="_blank">Github</a>
-                    <a href="#" id="doc-link" target="_blank">{{ __('admin.documentation') }}</a>
-                    <a href="#" id="demo-link" target="_blank">{{ __('admin.extensions') }}</a>
-                    <a href="https://jqhph.github.io/aio/demo.html" id="demo-link" target="_blank">{{ __('admin.demo') }}</a>
+                    <a href="{{ \Appsolutely\AIO\Admin::WEBSITE_URL }}" target="_blank">Website</a>
+                    <a href="{{ \Appsolutely\AIO\Admin::GITHUB_URL }}" target="_blank">Github</a>
                 </div>
             </div>
         </div>
--- resources/views/layouts/container.blade.php
+++ resources/views/layouts/container.blade.php
@@ -1,45 +1,48 @@
 <body
-        class="aio-body sidebar-mini layout-fixed {{ $configData['body_class']}} {{ $configData['sidebar_class'] }}
-        {{ $configData['navbar_class'] === 'fixed-top' ? 'navbar-fixed-top' : '' }} " >
+    class="aio-body sidebar-mini layout-fixed {{ $configData['body_class'] }} {{ $configData['sidebar_class'] }}
+        {{ $configData['navbar_class'] === 'fixed-top' ? 'navbar-fixed-top' : '' }} ">
 
-<script>
-    var AIO = CreateAIO({!! Appsolutely\AIO\Admin::jsVariables() !!});
-</script>
+    <script>
+        var AIO = CreateAIO({!! Appsolutely\AIO\Admin::jsVariables() !!});
+    </script>
 
-{!! admin_section(Appsolutely\AIO\Admin::SECTION['BODY_INNER_BEFORE']) !!}
+    {!! admin_section(Appsolutely\AIO\Admin::SECTION['BODY_INNER_BEFORE']) !!}
 
-<div class="wrapper">
-    @include('admin::partials.sidebar')
+    <div class="wrapper">
+        @include('admin::partials.sidebar')
 
-    @include('admin::partials.navbar')
+        @include('admin::partials.navbar')
 
-    <div class="app-content content">
-        <div class="content-wrapper" id="{{ $pjaxContainerId }}" style="top: 0;min-height: 900px;">
-            @yield('app')
+        <div class="app-content content">
+            <div class="content-wrapper" id="{{ $pjaxContainerId }}" style="top: 0;min-height: 900px;">
+                @yield('app')
+            </div>
         </div>
     </div>
-</div>
 
-<footer class="main-footer pt-1">
-    <p class="clearfix blue-grey lighten-2 mb-0 text-center">
+    <footer class="main-footer pt-1">
+        <p class="clearfix blue-grey lighten-2 mb-0 text-center">
             <span class="text-center d-block d-md-inline-block mt-25">
                 Powered by
-                <a target="_blank" href="https://appsolutely.io">Appsolutely AIO</a>
+                <a target="_blank" href="{{ Appsolutely\AIO\Admin::WEBSITE_URL }}">Appsolutely AIO</a>
                 <span>&nbsp;·&nbsp;</span>
                 v{{ Appsolutely\AIO\Admin::VERSION }}
             </span>
 
-        <button class="btn btn-primary btn-icon scroll-top pull-right" style="position: fixed;bottom: 2%; right: 10px;display: none">
-            <i class="feather icon-arrow-up"></i>
-        </button>
-    </p>
-</footer>
+            <button class="btn btn-primary btn-icon scroll-top pull-right"
+                style="position: fixed;bottom: 2%; right: 10px;display: none">
+                <i class="feather icon-arrow-up"></i>
+            </button>
+        </p>
+    </footer>
 
-{!! admin_section(Appsolutely\AIO\Admin::SECTION['BODY_INNER_AFTER']) !!}
+    {!! admin_section(Appsolutely\AIO\Admin::SECTION['BODY_INNER_AFTER']) !!}
 
-{!! Appsolutely\AIO\Admin::asset()->jsToHtml() !!}
+    {!! Appsolutely\AIO\Admin::asset()->jsToHtml() !!}
 
-<script>AIO.boot();</script>
+    <script>
+        AIO.boot();
+    </script>
 
 </body>
 
